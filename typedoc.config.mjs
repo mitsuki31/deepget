06107db3cbb9400eb54d8d39586d256739cf3702
Ryuu Mitsuki <dhefam31@gmail.com>
2025-02-16 01:30:18 +0700

    chore: Initial TypeDoc configuration

---
+++ typedoc.config.mjs
@@ -0,0 +1,41 @@
+import pkg from './package.json';
+
+/** @type {import('typedoc').TypeDocOptions} */
+const config = {
+  name: pkg.title,
+  includeVersion: true,
+  titleLink: pkg.repository.url.replace(/^git\+/, ''),
+  entryPoints: [
+    'src/index.ts'
+  ],
+  out: './docs',
+  readme: './README.md',
+  tsconfig: './tsconfig.json',
+  plugin: [ '@typhonjs-typedoc/typedoc-theme-dmt', 'typedoc-plugin-rename-defaults' ],
+  theme: 'default-modern',
+  cacheBust: true,
+  sort: true,
+  useTsLinkResolution: false,
+  gitRemote: 'origin',
+  logLevel: 'Verbose',
+
+  // @typhonjs-typedoc/typedoc-theme-dmt Configuration
+  dmtFavicon: undefined,
+  dmtLinksService: {
+    GitHub: 'https://github.com/mitsuki31/deepget',
+    NPM: 'https://npmjs.com/package/deepget'
+  },
+  dmtNavigation: {
+    moduleIcon: true,
+    style: 'full'
+  },
+  dmtSearch: {
+    fullName: false,
+    limit: 10
+  },
+  dmtSettings: {
+    animation: true
+  }
+};
+
+export default config;
